9b3eaebff367df7c87dba722a2fec5ac939514f5
Remyson <remysonrodrigues@gmail.com>
2022-05-03 11:44:45 -0300

    add camada de repository

---
+++ src/main/java/com/dio/santander/bankline/api/repository/CorrentistaRepository.java
@@ -0,0 +1,9 @@
+package com.dio.santander.bankline.api.repository;
+
+import com.dio.santander.bankline.api.model.Correntista;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CorrentistaRepository extends JpaRepository<Correntista, Integer> {
+}
